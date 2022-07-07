a968485878d956c6b90aab09800eaa6e1059fe2d
vionaosiako <viona.osiako@student.morigaschool.com>
2022-07-07 20:11:58 +0300

    Resolve the errors

---
+++ authentification/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('Image')
+    bio= models.TextField(max_length=355,default='my bio',blank=True)
+    name = models.CharField(max_length=65,blank=True)
+    email = models.EmailField(max_length=120,blank=True)
+    location = models.CharField(max_length=65,blank=True)
+    contact = models.CharField(max_length=65,blank=True) 
+    
+    def __str__(self):
+        
+        return self.user.username
+
+    
+    @receiver(post_save,sender=User)
+    def create_user_profile(sender, instance, created,**kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance,**kwargs):
+        instance.profile.save()
+    
